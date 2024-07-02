6303871734d46161e3b8f468f13936917dd3556c
unknown <r.kouchoo1@gmail.com>
2024-07-02 22:24:39 +1000

    shitty idea of a logger

--- src/main.py
+++ src/main.py
@@ -1,11 +1,5 @@
-import bz2
-import glob
-#import satpy
-#import s3fs
-
 from s3 import s3StringBuilder
 from s3 import s3DownloadManager
-from s3 import s3DateCarrier
 from sats import satellites
 
 # https://sites.google.com/view/raybellwaves/cheat-sheets/satpy
@@ -19,6 +13,8 @@ attribs = himawarisat.getAttributes()
 uri = s3StringBuilder.buildLatestS3QueryURI(sat=himawarisat, prod=attribs.L1.FULL_DISK) # type: ignore
 #ags = s3DownloadManager.getLatestDataFromS3("noaa-himawari9/AHI-L1b-FLDK/2024/07/02/1040/", saTime=s3DateCarrier.carrier("2024", "07", "02", "1040"), satellite=himawarisat)
 ags = s3DownloadManager.getLatestDataFromS3(uri[0], uri[1], satellite=himawarisat) # type: ignore
+
+
 
 '''
     filenamez = glob.glob("glob/decom/*.DAT")
