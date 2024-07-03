6ee726a9ca06091b50cd0bd841777ada9f533609
Regan Kouchoo <regan.kouchoo@netway.net.au>
2024-07-03 18:04:27 +1000

    thredds

--- src/thredds-test.py
+++ src/thredds-test.py
@@ -17,19 +17,22 @@ def downloadWrapper(path, urls):
         print("Downloaded: {}".format(totalpath))
     print("Thread exit. Total: {} ".format(len(urls)))
 
+test = threddsclient.download_urls("https://thredds.nci.org.au/thredds/catalog/gc63/satellite-products/nrt/raw/himawari-ahi/fldk/latest/2024/07/03/0710/catalog.xml")
 
-if (int(len(urls)) % 2) == 0: 
+if False:
+     
+    if (int(len(urls)) % 2) == 0: 
+            
+            # lets split all the files into 4 chunks for a 4 threadded download and extract
+            divCount = int(len(urls) / 4)
+            
+            with concurrent.futures.ThreadPoolExecutor(max_workers=4) as pool:
+                th1 = pool.submit(downloadWrapper, datPath, urls[:divCount])
+                th2 = pool.submit(downloadWrapper, datPath, urls[divCount:divCount*2])
+                th3 = pool.submit(downloadWrapper, datPath, urls[divCount*2:divCount*3])
+                th4 = pool.submit(downloadWrapper, datPath, urls[divCount*3:divCount*4])
         
-        # lets split all the files into 4 chunks for a 4 threadded download and extract
-        divCount = int(len(urls) / 4)
-        
-        with concurrent.futures.ThreadPoolExecutor(max_workers=4) as pool:
-            th1 = pool.submit(downloadWrapper, datPath, urls[:divCount])
-            th2 = pool.submit(downloadWrapper, datPath, urls[divCount:divCount*2])
-            th3 = pool.submit(downloadWrapper, datPath, urls[divCount*2:divCount*3])
-            th4 = pool.submit(downloadWrapper, datPath, urls[divCount*3:divCount*4])
-    
-        pool.shutdown(wait=True)
+            pool.shutdown(wait=True)
 
 
             
