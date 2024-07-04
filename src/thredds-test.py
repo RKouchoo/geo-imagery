5524047dfbecefc8d2196153d0dcacfc1634379d
unknown <r.kouchoo1@gmail.com>
2024-07-04 21:48:01 +1000

    thredds download example

--- src/thredds-test.py
+++ src/thredds-test.py
@@ -1,22 +1,11 @@
 import concurrent.futures
 import urllib.request
 
-import threddsclient
 
-from remote import dateUtil
-
-datPath = "../data/thredds/"
+from remote import threddsBuilder
 
 
-def genThreddsUrl():
-     dd = dateUtil.getLatestDateCarrier(negativeOffset=0)
-     url = "https://thredds.nci.org.au/thredds/catalog/gc63/satellite-products/nrt/raw/himawari-ahi/fldk/latest/{}/catalog.xml".format(dd.getStdQueryString())
-     dd.setQueryURI(url)
-     print(url)
-     return dd
-     
-
-def downloadWrapper(path, urls):
+def download(path, urls):
     for url in urls:
         fname = url.split("/")
         fname = fname[len(fname) - 1]
@@ -26,24 +15,31 @@ def downloadWrapper(path, urls):
     print("Thread exit. Total: {} ".format(len(urls)))
 
 
-obj = genThreddsUrl()
-test = threddsclient.download_urls(obj.getQueryURI())
-print(len(test))
 
-if False:
-     
-    if (int(len(urls)) % 2) == 0: 
+# downloads every dataset that was uploaded today (up until now)
+
+def downloadToday():
+
+    datPath = "../data/thredds/"
+    day = threddsBuilder.getThreddsDayURI()
+    dataset = threddsBuilder.getThreddsCompleteDataset(day.getQueryURI())
+
+    for d in dataset:
+        urls = threddsBuilder.getThreddsDownloadURLs(d.getQueryURI())
+        datPath = "../data/thredds/{}/{}/".format("himawari9", d.getCompleteDateString())
+
+        if (int(len(urls)) % 2) == 0: 
+                
+                # lets split all the files into 4 chunks for a 4 threadded download and extract
+                divCount = int(len(urls) / 4)
+                
+                with concurrent.futures.ThreadPoolExecutor(max_workers=4) as pool:
+                    th1 = pool.submit(download, datPath, urls[:divCount])
+                    th2 = pool.submit(download, datPath, urls[divCount:divCount*2])
+                    th3 = pool.submit(download, datPath, urls[divCount*2:divCount*3])
+                    th4 = pool.submit(download, datPath, urls[divCount*3:divCount*4])
             
-            # lets split all the files into 4 chunks for a 4 threadded download and extract
-            divCount = int(len(urls) / 4)
-            
-            with sconcurrent.futures.ThreadPoolExecutor(max_workers=4) as pool:
-                th1 = pool.submit(downloadWrapper, datPath, urls[:divCount])
-                th2 = pool.submit(downloadWrapper, datPath, urls[divCount:divCount*2])
-                th3 = pool.submit(downloadWrapper, datPath, urls[divCount*2:divCount*3])
-                th4 = pool.submit(downloadWrapper, datPath, urls[divCount*3:divCount*4])
-        
-            pool.shutdown(wait=True)
+                pool.shutdown(wait=True)
 
 
-            
+
