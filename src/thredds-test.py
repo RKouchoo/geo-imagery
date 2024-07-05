b5baab5a68383039f36668d8b849a534cefdc030
Regan Kouchoo <regan.kouchoo@netway.net.au>
2024-07-05 12:26:32 +1000

    thredds bug fix

--- src/thredds-test.py
+++ src/thredds-test.py
@@ -5,7 +5,7 @@ import os
 from remote import threddsBuilder
 from remote import dateCarrier
 
-def download(path, urls):
+def downloadList(path, urls):
     if not os.path.exists(path):
         os.makedirs(path)
     for url in urls:
@@ -18,34 +18,28 @@ def download(path, urls):
     print("Thread exit. Total: {} ".format(len(urls)))
 
 
-# downloads every dataset that was uploaded today (up until now)
-
-def downloadToday(day=threddsBuilder.getThreddsDayURI()):
-
+# downloads every dataset that was uploaded for the date range (up until now)
+# day = custom date via carrier class
+def downloadCompleteDataset(day=threddsBuilder.getThreddsDayURI()):
     datPath = "../data/thredds/"
-    day = threddsBuilder.getThreddsDayURI()
-    print(day.getQueryURI())
     dataset = threddsBuilder.getThreddsCompleteDataset(day.getQueryURI())
 
     for d in dataset:
         urls = threddsBuilder.getThreddsDownloadURLs(d.getQueryURI())
         datPath = "../data/thredds/{}/{}/".format("himawari9", d.getCompleteDateString())
         
-        if len(urls) < 160:
-            pass
-
-        if True: 
-                # lets split all the files into 4 chunks for a 4 threadded download and extract
-                divCount = int(len(urls) / 4)
+        if len(urls) == 160:
+            # lets split all the files into 4 chunks for a 4 threadded download and extract
+            divCount = int(len(urls) / 4)
                 
-                with concurrent.futures.ThreadPoolExecutor(max_workers=4) as pool:
-                    th1 = pool.submit(download, datPath, urls[:divCount])
-                    th2 = pool.submit(download, datPath, urls[divCount:divCount*2])
-                    th3 = pool.submit(download, datPath, urls[divCount*2:divCount*3])
-                    th4 = pool.submit(download, datPath, urls[divCount*3:divCount*4])
+            with concurrent.futures.ThreadPoolExecutor(max_workers=4) as pool:
+                th1 = pool.submit(downloadList, datPath, urls[:divCount])
+                th2 = pool.submit(downloadList, datPath, urls[divCount:divCount*2])
+                th3 = pool.submit(downloadList, datPath, urls[divCount*2:divCount*3])
+                th4 = pool.submit(downloadList, datPath, urls[divCount*3:divCount*4])
             
-                pool.shutdown(wait=True)
+            pool.shutdown(wait=True)
 
 
 
-downloadToday(day=threddsBuilder.getThreddsDayCustomURI(dateCarrier.carrier("2024", "07", "04", "00", True)))
+downloadCompleteDataset(day=threddsBuilder.getThreddsDayCustomURI(dateCarrier.carrier("2024", "07", "04", "00", True)))
