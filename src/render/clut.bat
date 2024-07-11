e41df4a4954738f7a4527ff81ae5e989af91c73c
unknown <r.kouchoo1@gmail.com>
2024-07-11 10:40:55 +1000

    rendering

--- src/render/clut.bat
+++ src/render/clut.bat
@@ -1,7 +1,9 @@
 echo off
 REM arg %1 is the path to the file
+echo %1
+set out = %1_clut.png
 
-convert %1 ( -clone 0 -colorspace CMYK -channel CMY -separate -evaluate-sequence add ) ( -clone 0,1 -alpha off -compose copyopacity -composite ) ( -clone 0 -fuzz 15% +transparent white -blur 0x1.5 ) -delete 0,1 -compose over -composite %1_clut.png
+convert %1 ( -clone 0 -colorspace CMYK -channel CMY -separate -evaluate-sequence add ) ( -clone 0,1 -alpha off -compose copyopacity -composite ) ( -clone 0 -fuzz 15%% +transparent white -blur 0x1.5 ) -delete 0,1 -compose over -composite %out%
 
 REM remove the trailing edges of the image (colour #9E0142)
-convert %1_clut.png  -fuzz 20% -transparent "#9E0142" %1_clut.png
+convert %out%  -fuzz 20%% -transparent "#9E0142" %out%
--- src/render/renderDual.py
+++ src/render/renderDual.py
@@ -2,14 +2,33 @@ import os
 import glob 
 
 import subprocess
+import shutil
 
-geoDir = ""
-clutDir = ""
+geoDir = os.path.abspath("../../source_render/true_color_reproduction_night_ir")
+clutDir = os.path.abspath("../../source_render/colorized_ir_clouds/")
+clutDest = "proc"
 
 geos = glob.glob(geoDir + "*.png")
-weathers = glob.glob(clutDir + "*.png")
+weathers = glob.iglob(clutDir + '/*.png')
+
+cluts = []
+
+
+def doClutRender(path, source, dest):
+	newpath = "{}\\{}\\{}".format(source, dest, os.path.basename(path))
+	shutil.copyfile(path, newpath)
+	clutArgs = "convert {} ( -clone 0 -colorspace CMYK -channel CMY -separate -evaluate-sequence add ) ( -clone 0,1 -alpha off -compose copyopacity -composite ) ( -clone 0 -fuzz 15%% +transparent white -blur 0x1.5 ) -delete 0,1 -compose over -composite {}".format(newpath, newpath)
+	fixClutArgs = 'convert {}  -fuzz 20% -transparent "#9E0142" {}'.format(newpath, newpath)
+	os.system(clutArgs)
+	os.system(fixClutArgs)
+	return newpath
+	
+	
+def doImageMerge(geo, weather, result):
+	args = "composite -compose over {} {} {}".format(weather, geo, result)
+	return result
+	
+
 
 for weather in weathers:
-        weatherArgs = ["clut.bat", weather]
-        subprocess.call(weatherArgs)
-
+	cluts.append(doClutRender(weather, clutDir, clutDest))
